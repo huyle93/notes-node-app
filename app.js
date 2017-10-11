b8c6b9b0053eebe7e4ca3d9e8ea5b96954c8bee4
Huy Le <huyle.finance@gmail.com>
2017-10-11 00:37:49 -0400

    working yargs input and console.log

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ console.log('starting app');
 const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes.js');
+const yargs = require('yargs');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+var command = argv._[0]
 console.log('Command: ', command);
-console.log(process.argv);
-
+console.log('Process',process.argv);
+console.log('Yargs', argv)
 //process.argv helps grabbing input from user (command line argument)
 
 if (command === 'add') {
-    console.log('Adding new note');
+    // console.log('Adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('Listing all notes');
+    // console.log('Listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('Reading note');
+    // console.log('Reading note');
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('Remove note')
+    // console.log('Remove note')
+    notes.removeNote(argv.title)
 } else {
     console.log('Command not recognized!');
 }
--- notes.js
+++ notes.js
@@ -1,9 +1,27 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
+var addNote = (title, body) => {
+    console.log('Adding note', title, body);
 };
 
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+}
+
+module.exports = {
+    addNote,
+    // addNote above in ES6 is addNote: addNote which point to addNote function above
+    getAll,
+    getNote,
+    removeNote
+};
 
 
