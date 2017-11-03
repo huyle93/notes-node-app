3bb4957e4f676bc677fc55486e3fb14a83b0b20b
Huy Le <huyle.finance@gmail.com>
2017-11-03 16:00:43 -0400

    still developing removeNote function

--- notes.js
+++ notes.js
@@ -44,7 +44,12 @@ var getNote = (title) => {
 };
 
 var removeNote = (title) => {
-    console.log('Removing note', title);
+    // fetch notes
+    var notes = fetchNotes();
+    // filter notes, removing the one with title of argument
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    // save new notes array
+    saveNotes(filteredNotes);
 };
 
 module.exports = {
